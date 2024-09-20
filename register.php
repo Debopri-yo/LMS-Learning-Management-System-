cc8ec3bb8867337d1ef8a5ee1b1d06571eadf313
Debopri-yo <ghoshlipika53@gmail.com>
2024-09-20 21:32:22 +0530

    added the readme and banner,register(html,css)page

---
+++ register.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="banner">
+            <img src="banner2.JPG" alt="banner of NITMAS"
+        </div>
+         
+        </div>
+        <div class="container">
+        <div class="row text-center mt-5">
+            <div class="col-md-3">
+                <a href="#" class="btn btn-primary btn-lg btn-block custom-btn">
+                    <i class="fas fa-user"></i> Student Login
+                </a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class="btn btn-warning btn-lg btn-block custom-btn">
+                    <i class="fas fa-university"></i> Admin Login
+                </a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class="btn btn-danger btn-lg btn-block custom-btn">
+                    <i class="fas fa-chalkboard-teacher"></i> Teacher Login
+                </a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class="btn btn-dark btn-lg btn-block custom-btn">
+                    <i class="fas fa-school"></i> College Login
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <!-- FontAwesome for icons -->
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    </body>
+</html>
